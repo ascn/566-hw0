5a4fc942c582d14eed66a56c2e0ef312efaac7fb
Alexander Chan <alexanderchan97@gmail.com>
2019-01-20 23:07:53 -0500

    Submission

---
+++ src/geometry/Cube.ts
@@ -0,0 +1,59 @@
+import {vec3, vec4} from 'gl-matrix';
+import Drawable from '../rendering/gl/Drawable';
+import {gl} from '../globals';
+
+class Cube extends Drawable {
+	buffer: ArrayBuffer;
+	indices: Uint32Array;
+	positions: Float32Array;
+	normals: Float32Array;
+	center: vec4;
+
+	constructor() {
+		super();
+	}
+
+	create() {
+		this.positions = new Float32Array([
+			-0.5, 0.5, -0.5, 0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5,
+			-0.5, -0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5,
+			0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+			-0.5, 0.5, -0.5, 0.5, -0.5, -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5,
+			-0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5,
+			-0.5, 0.5, -0.5, 0.5, 0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, -0.5, -0.5, -0.5, 0.5
+		]);
+		this.normals = new Float32Array([
+			0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0,
+			0, -1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0,
+			1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0,
+			-1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0,
+			0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0,
+			0, 0, -1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, -1, 0
+		]);
+		this.indices = new Uint32Array([
+			0, 1, 2, 0, 2, 3,
+			4, 5, 6, 4, 6, 7,
+			8, 9, 10, 8, 10, 11,
+			12, 13, 14, 12, 14, 15,
+			16, 17, 18, 16, 18, 19,
+			20, 21, 22, 20, 22, 23
+		]);
+		this.generateIdx();
+		this.generatePos();
+		this.generateNor();
+
+		this.count = this.indices.length;
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.bufIdx);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indices, gl.STATIC_DRAW);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.bufNor);
+		gl.bufferData(gl.ARRAY_BUFFER, this.normals, gl.STATIC_DRAW);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.bufPos);
+		gl.bufferData(gl.ARRAY_BUFFER, this.positions, gl.STATIC_DRAW);
+
+		console.log(`Created cube`);
+	}
+};
+
+export default Cube;
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,40 @@
-import {vec3} from 'gl-matrix';
+import {vec3, vec4} from 'gl-matrix';
 import * as Stats from 'stats-js';
 import * as DAT from 'dat-gui';
 import Icosphere from './geometry/Icosphere';
 import Square from './geometry/Square';
+import Cube from './geometry/Cube';
 import OpenGLRenderer from './rendering/gl/OpenGLRenderer';
 import Camera from './Camera';
 import {setGL} from './globals';
 import ShaderProgram, {Shader} from './rendering/gl/ShaderProgram';
+
+let icosphere: Icosphere;
+let square: Square;
+let cube: Cube;
+let prevTesselations: number = 5;
 
 // Define an object with application parameters and button callbacks
 // This will be referred to by dat.GUI's functions that add GUI elements.
 const controls = {
   tesselations: 5,
   'Load Scene': loadScene, // A function pointer, essentially
+  'color': [128, 128, 128],
+  'shader': 'morph',
+  'shape': 'cube'
 };
-
-let icosphere: Icosphere;
-let square: Square;
-let prevTesselations: number = 5;
 
 function loadScene() {
   icosphere = new Icosphere(vec3.fromValues(0, 0, 0), 1, controls.tesselations);
   icosphere.create();
   square = new Square(vec3.fromValues(0, 0, 0));
   square.create();
+  cube = new Cube();
+  cube.create();
 }
+
+let prevTime: number;
+let time: number = 0;
 
 function main() {
   // Initial display for framerate
@@ -39,6 +49,9 @@ function main() {
   const gui = new DAT.GUI();
   gui.add(controls, 'tesselations', 0, 8).step(1);
   gui.add(controls, 'Load Scene');
+  gui.addColor(controls, 'color');
+  gui.add(controls, 'shader', ['lambert', 'morph']);
+  gui.add(controls, 'shape', ['square', 'cube', 'icosphere']);
 
   // get canvas and webgl context
   const canvas = <HTMLCanvasElement> document.getElementById('canvas');
@@ -64,9 +77,24 @@ function main() {
     new Shader(gl.FRAGMENT_SHADER, require('./shaders/lambert-frag.glsl')),
   ]);
 
+  const morph = new ShaderProgram([
+    new Shader(gl.VERTEX_SHADER, require('./shaders/morph-vert.glsl')),
+    new Shader(gl.FRAGMENT_SHADER, require('./shaders/lambert-frag.glsl'))
+  ]);
+
   // This function will be called every frame
   function tick() {
     camera.update();
+    lambert.setGeometryColor(vec4.fromValues(controls.color[0] / 255,
+                                             controls.color[1] / 255,
+                                             controls.color[2] / 255,
+                                             1));
+    morph.setGeometryColor(vec4.fromValues(controls.color[0] / 255,
+                                           controls.color[1] / 255,
+                                           controls.color[2] / 255,
+                                           1));
+    time += new Date().getTime() - prevTime;
+    morph.setTime(time / 480000.);
     stats.begin();
     gl.viewport(0, 0, window.innerWidth, window.innerHeight);
     renderer.clear();
@@ -76,9 +104,9 @@ function main() {
       icosphere = new Icosphere(vec3.fromValues(0, 0, 0), 1, prevTesselations);
       icosphere.create();
     }
-    renderer.render(camera, lambert, [
-      icosphere,
-      // square,
+    renderer.render(camera, controls.shader == 'lambert' ? lambert : morph, [
+      controls.shape == 'cube' ? cube :
+      controls.shape == 'square' ? square : icosphere
     ]);
     stats.end();
 
@@ -95,6 +123,8 @@ function main() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.setAspectRatio(window.innerWidth / window.innerHeight);
   camera.updateProjectionMatrix();
+
+  prevTime = new Date().getTime();
 
   // Start the render loop
   tick();
